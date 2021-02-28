5c406238e932bc29d512619a9d2bc98dcd327c7c
Бернова Анна <bast33@yandex.ru>
2021-02-28 18:42:48 +0300

    add optins and format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,8 @@ import program from 'commander';
 
 program
   .version('0.1.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'Output format', 'stylish')
   .description('Compares two configuration files and shows a difference.')
   .action(() => {
     console.log('It`s alive!');
